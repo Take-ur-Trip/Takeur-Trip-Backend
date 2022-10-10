239dc2d77c96da791ea7c9136bdec61911b4cbe5
Nikodem <nikodem.zychma@gmail.com>
2022-10-10 23:39:15 +0200

    feat: jwt auth

---
+++ middlewares/jwt.ts
@@ -0,0 +1,25 @@
+import jwt, { Secret, JwtPayload} from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import config from '../config.json';
+require('dotenv').config();
+
+export interface JwtPayloadRequest extends Request {
+    token: string | JwtPayload
+}
+
+export const jwtAuth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.header('x-access-token') as string || req.header('Authorization') as string;
+        const secretKeyJwt : Secret = process.env.JWT_SECRETKEY as Secret;
+        if(!token) {
+            throw new Error(`${config.messages.tokenInvalid}`);
+        }
+
+        const payload = jwt.verify(token, secretKeyJwt);
+        (req as JwtPayloadRequest).token = payload;
+    
+        next();
+    } catch(err) {
+        res.json(config.messages.tokenInvalid)
+    }
+}
--- routes/users.ts
+++ routes/users.ts
@@ -6,8 +6,12 @@ import bcrypt from 'bcrypt';
 import { sanitizeString } from "../middlewares/sanitizeString";
 import { generateRandomHash, sendMail } from "../middlewares/mailer";
 import { QueryResult } from "pg";
+import jwt, { Secret } from 'jsonwebtoken';
+import { jwtAuth } from "../middlewares/jwt";
+require('dotenv').config();
 
 const router = Router();
+const secretKeyJwt : Secret = process.env.JWT_SECRETKEY as Secret;
 
 type CredentialsModel = {
     email : string,
@@ -31,7 +35,9 @@ router.post('/register', async (req: Request<CredentialsModel>, res: Response) =
             const verifyHash = await generateRandomHash(30);
             await sendMail(user.email, verifyHash);
             await query(`UPDATE "public.Users" SET verification=$1 WHERE email LIKE $2`, [verifyHash, user.email]);
-            res.json(config.messages.registredUser);
+            const token : string = jwt.sign(user.email, secretKeyJwt);
+            const tokenObj : Object = { token };
+            res.json({...config.messages.registredUser, ...tokenObj});
         } else {
             res.json(config.messages.couldntRegisterUser);
         }
@@ -55,6 +61,26 @@ router.get('/verify', async (req : Request, res: Response) => {
             } else {
                 res.json(config.messages.verifySuccessful);
             }
+        }
+    }
+})
+
+// Auth
+
+router.post('/auth', async (req : Request, res : Response) => {
+    const email = req.body.email as string;
+    const password = req.body.password as string;
+    const { rows : emailRows} = await query(`SELECT email, password FROM "public.Users" WHERE email LIKE $1`, [email]);
+    if(emailRows.length < 1) {
+        res.json(config.messages.authIncorrectCredentials);
+    } else {
+        const encryptedPassword = await bcrypt.compare(password, emailRows[0].password);
+        if(!encryptedPassword) {
+            res.json(config.messages.authIncorrectCredentials);
+        } else {
+            const token : string = jwt.sign(emailRows[0].email, secretKeyJwt);
+            const tokenObj : Object = { token };
+            res.json({...config.messages.authSuccess, ...tokenObj})
         }
     }
 })
